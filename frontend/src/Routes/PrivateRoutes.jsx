88fd565abbe1c9380a1bde8fd8ff5a428fb10f3c
develop <patinetakiller564@gmail.com>
2025-11-28 15:11:08 -0300

    Me habia olvidado de verificar las rutas publicas y privadas, practicamente se copio lo del segundo parcial acerca de las funciones con los ternarios

--- frontend/src/Routes/PrivateRoutes.jsx
+++ frontend/src/Routes/PrivateRoutes.jsx
@@ -1,9 +1,36 @@
-export default function PrivateRoutes() {
-  return (
-    <>
-    <div>
-      <h1>Private Routes</h1>
-    </div>
-    </>
+import { useEffect, useState } from "react";
+import { Loading } from "../components/Loading";
+import { Outlet, Navigate } from "react-router-dom";
+export const PrivateRoute = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const checkAut = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/profile", {
+        credentials: "include",
+      });
+      if (response.ok) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+    } catch (error) {
+      console.log("Error de fetch");
+      setIsAuthenticated(false);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    checkAut();
+  }, []);
+  return isLoading ? (
+    <Loading />
+  ) : isAuthenticated ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/home" />
   );
-}
+};
--- frontend/src/Routes/PublicRoutes.jsx
+++ frontend/src/Routes/PublicRoutes.jsx
@@ -1,9 +1,38 @@
-import Login from "../Pages/Login";
+import { Outlet, Navigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Loading } from "../components/Loading";
 
-export default function PublicRoutes() {
-  return (
-    <div>
-      <Login />
-    </div>
+export default function PublicRoute() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const checkAut = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/profile", {
+        credentials: "include",
+      });
+      if (response.ok) {
+        // si la respuesta es ok es porque está autenticado
+        isAuthenticated(true);
+      } else {
+        //   si no, no está autenticado
+        setIsAuthenticated(false);
+      }
+    } catch (error) {
+      // console.log("Error de fetch");
+      setIsAuthenticated(false);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    checkAut();
+  }, []);
+  return isLoading ? (
+    <Loading />
+  ) : isAuthenticated ? (
+    <Navigate to="/home" />
+  ) : (
+    <Outlet />
   );
 }
